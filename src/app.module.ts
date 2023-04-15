07fc77bab680397df9edb46173158339c4f1ff58
dalandis <varganov.a.p@gmail.com>
2023-04-15 12:44:24 +0500

    admins

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,7 +26,7 @@ import { ScheduleModule } from '@nestjs/schedule';
     imports: [
         ServeStaticModule.forRoot({
             rootPath: join(__dirname, '../..', 'gmgame-site', 'build'),
-            exclude: ['(.*)'],
+            // exclude: ['(.*)'],
         }),
         ConfigModule.forRoot({
             envFilePath: '.env'
