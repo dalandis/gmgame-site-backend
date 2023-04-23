dab249ba038d6b3f91838de2ffe35d2414f08682
dalandis <varganov.a.p@gmail.com>
2023-04-23 22:01:16 +0500

    fix locations

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,11 @@ import * as basicAuth from 'express-basic-auth';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+        origin: 'https://map.gmgame.ru',
+        credentials: true
+    });
+
     app.use(cookieSession({
         name: 'session',
         keys: [process.env.SECRET_SESSION],
